3b4758fb64a02c40bc6fdaff3c6a7d53904acf8a
Justin Thomas <jthoma59@uic.edu>
2020-03-18 16:21:20 -0500

    Revert "added route urls"
    This reverts commit 307043b9cc2fe3cfd23d214163839b6f12af1639.

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,20 @@
+
 import React, { Component } from 'react';
 import LoginPage from './logout/login'
 import SelectionPage from './login/selection'
 import MacroCalculator from './calculator/calculator'
 import NutritionTracker from './tracker/tracker'
-import Grid from './grid/grid'
-import { BrowserRouter as Router } from 'react-router-dom'
-import Route from 'react-router-dom/Route'
+
 
 class App extends Component {
   render() {
-
+    //return <div>StayFit! Application<p>Fun stuff to come!</p></div>;
+  
       return(
-
-        //add routes to each component
-        <Router>
-          <div className="App">
-            <Route path='/' exact component={LoginPage} /> 
-            <Route path='/logout' component={LoginPage} />
-            <Route path='/selection' component={SelectionPage} />   
-            <Route path='/calculator' component={MacroCalculator} />  
-            <Route path='/grid' component={Grid} />  
-            <Route path='/tracker' component={NutritionTracker} />  
-          </div>
-        </Router>
-
-        )
+        <div className="App">
+          <LoginPage />
+        </div>
+      )
   
   }
 }
