9ad423cf1f726c3c4ccec5e9ce8a15cd7879e750
SaemaAnsari <ansari.saema@gmail.com>
2020-03-22 00:18:57 -0500

    Created a macrosSchema and updated userSchema to include macrosSchema

--- server/models/user.js
+++ server/models/user.js
@@ -2,9 +2,16 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const macrosSchema = new Schema({
+  prots: {type: typeof(2.33333333), require: true},
+  carbs: {type: typeof(2.33333333), require: true},
+  fats: {type: typeof(2.33333333), require: true}
+});
+
 const userSchema = new Schema({
   username: { type: String, require: true, unique: true },
-  password: { type: String, require: true }
+  password: { type: String, require: true },
+  macros: { type: macrosSchema, require: false }
 }, { timestamps: true }
 );
 
