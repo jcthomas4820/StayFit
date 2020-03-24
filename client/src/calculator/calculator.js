ec4113fd63ef9693c48e0632301b0ace5a454823
Justin Thomas <jthoma59@uic.edu>
2020-03-23 20:00:54 -0500

    pseudo-merge master and justin

--- client/src/calculator/calculator.js
+++ client/src/calculator/calculator.js
@@ -1,5 +1,11 @@
+/*
+Form written as controlled component:
+    MacroCalculator component randers the form and controls what happens in that form
+*/
+
 import React from "react"
 import axios from 'axios'
+import { thisExpression } from "@babel/types"
 
 //  allow user to calculate their recommended daily macros
 class MacroCalculator extends React.Component{
@@ -7,82 +13,175 @@ class MacroCalculator extends React.Component{
     constructor(){
         super()
         this.state={
-            data: {prot: null,
-                   carbs: null,
-                   fats: null},
-            calc: false,
-            submit: false,
-            back: false,
-            userAge: null,
-            userGender: null,
-            userWeight: null,
-            userHeight: null,
-            userActivityLevel: null
+            gender: "",
+            age: "",
+            weight: "",
+            height: "",
+            activityLevel: "",
+            carbsG: "",
+            protG: "",
+            fatG: "",
+            results: ""
         }
+       
+        this.handleChange = this.handleChange.bind(this)
         this.handleClick = this.handleClick.bind(this)
+        this.clearForm = this.clearForm.bind(this)
     }
 
+    //  function handles text changes in textbox on form
+        //  allows any form changes to appear in the form
+    handleChange(e){
 
+        const name = e.target.name              //  name of html element changed
+        const value = e.target.value            //  value of html element changed
+
+        switch(name){
+
+            case "gender":
+                this.setState({gender: value})
+                //console.log("Setting State: " + this.state.gender)
+                break
+            case "age":
+                this.setState({age: value})
+                break
+            case "weight":
+                this.setState({weight: value})
+                break
+            case "height":
+                this.setState({height: value})
+                break
+            case "activityLevel":
+                this.setState({activityLevel: value})
+                break
+            //default:
+                //
+
+        }
+
+    }
+
+    //  function used to clear all values in the form except results
+    clearForm(){
+        document.getElementById("macro-calculator").reset()         //  reset radio buttons
+        this.setState({                 //  reset state
+            gender: "",
+            age: "",
+            weight: "",
+            height: "",
+            activityLevel: ""
+        })
+    }
+
+    //  function used to handle calculate and submission
     handleClick(e){
 
-        let id = e.target.id
-        if (id === 'calculate'){
-            let calories = 0;
-            system.out.println("here");
-            if(this.state.userGender === "Female"){
-                calories = 10*(this.state.userWeight) + (6.25 * this.state.userHeight) - 5*this.state.userAge - 161;
-            }
-            else if(this.state.userGender === "Male"){
-                calories = 10*(this.state.userWeight) + (6.25 * this.state.userHeight) - 5*this.state.userAge + 5;
-            }
-            const schema = {
-                carbs: calories*0.35,
-                prot: calories*0.35,
-                fats: calories*0.30
-            };
+        //  ensure state has all proper values (no neg, numbers not alpha, all values are entered, etc.)
+        //  if errors present, report in results field and return
 
-            axios.post('http://localhost:3000/calculator', schema);
-            //  grab user entered values
-            //  perform macro calculations using Mifflin-St. Jeor equation (https://www.healthline.com/nutrition/how-to-count-macros#step-by-step)
-            //  update state to reflect values calculated for macros  
-            //  update state for calc
-            this.setState({calc: true})
+        const button = e.target.name
+
+        if (button === "calculate"){
+
+            //  perform macro results calculation using Mifflin-St. Jeor equation (https://www.healthline.com/nutrition/how-to-count-macros#step-by-step)
+            const genderFactor = this.state.gender === "male" ? 5 : -161
+            let activityFactor=0
+            switch(this.state.activityLevel){
+                case "sedentary":
+                    activityFactor = 1.2
+                    break
+                case "lightly active":
+                    activityFactor = 1.375
+                    break
+                case "moderatively active":
+                    activityFactor = 1.55
+                    break
+                case "very active":
+                    activityFactor = 1.725
+                    break
+                case "extra active":
+                    activityFactor = 1.9
+                    break
+                //default:
+                    //
+            }
+
+            let calories = activityFactor*((10*this.state.weight)+(6.25*this.state.height)-(5*this.state.age)+genderFactor)
+            const carbs = Math.floor((0.5*calories)/4)
+            const fats = Math.floor((0.25*calories)/9)
+            const protein = Math.floor((0.25*calories)/4)
+
+            //  save in state results
+            this.setState({carbsG:carbs, fatsG:fats, protG:protein})
+            this.setState({results: carbs+" g carbs, " + fats+" g fats, " + protein+" g protein"})
         }
-        else if (id === 'submit'){
-            //  error check: ensure values are calculated before submission, by checking this.state.calc
-            //  grab this.state.data, store this data into database
-            //  let user know values were saved
-            //  clear the values on the screen (clear state values)
+
+
+
+
+        else if(button === "submit"){
+
+            //  check if calculated first
+
+            //  save state values in database (for use by Nutrition Tracker)
         }
+
 
     }
 
 
+//  note: radio buttons only allow one selection per name attribute
     render(){
 
         return(
-            <div className = "MacroCalculator">
+            <div className="MacroCalculator">
                 <h1>Macro Calculator</h1>
-                <div className = "UserValues">
-                    <input id="userGender" type="text" name="gender" value={this.state.userGender}/>
-                    <p>Gender (M/F)</p>
-                    <input id="userAge" type="number" name="age" value={this.state.userAge}/>
-                    <p>Age</p>
-                    <input id="userWeight" type="number" name="weight" value={this.state.userWeight}/>
-                    <p>Weight (cm)</p>
-                    <input id="userHeight" type="number" name="height" value={this.state.userHeight}/>
-                    <p>Height (lbs)</p>
-                    <input id="userActivityLevel" type="text" name="activity" value={this.state.userActivityLevel}/>
-                    <p>Activity Level (sedentary, lightly active, moderatively active, very active, extra active)</p>
-                </div>
-                <div className="Results">
-                    <input id="results" disabled type="text"></input>
-                </div>
-                <div className="CalcButtons">
-                    <button type="button" id="calculate" onClick={this.handleClick}>calculate</button>
-                    <button type="button" id="submit" onClick={this.handleClick}>submit</button>
-                </div>
-            </div>
+                
+                <form id="macro-calculator" >
+                    
+                    <label>
+                        Gender:<br/>
+                        <input name="gender" type="radio" value="male" onChange={this.handleChange} /> Male
+                        <input name="gender" type="radio" value="female" onChange={this.handleChange} /> Female
+                    </label>
+                    <br/>
+                    <label>
+                        Age (yr):<br/>
+                        <input name="age" type="number" value={this.state.age} onChange={this.handleChange} />
+                    </label>
+                    <br/>
+                    <label>
+                        Weight (kg):<br/>
+                        <input name="weight" type="number" value={this.state.weight} onChange={this.handleChange} />
+                    </label>
+                    <br/>
+                    <label>
+                        Height (cm):<br/>
+                        <input name="height" type="number" value={this.state.height} onChange={this.handleChange} />
+                    </label>
+                    <br/>
+                    <label>
+                        Activity Level:<br/>
+                        <input name="activityLevel" type="radio" value="sedentary" onChange={this.handleChange} /> Sedentary
+                        <input name="activityLevel" type="radio" value="lightly active" onChange={this.handleChange} /> Lightly Active
+                        <input name="activityLevel" type="radio" value="moderatively active" onChange={this.handleChange} /> Moderatively Active
+                        <input name="activityLevel" type="radio" value="very active" onChange={this.handleChange} /> Very Active
+                        <input name="activityLevel" type="radio" value="extra active" onChange={this.handleChange} /> Extra Active
+                    </label>
+
+                    <br/>
+                    <div onClick={this.handleClick}>
+                        <input type="button" name="calculate" value="calculate" />
+                        <input type="button" name="submit" value="submit" />
+                    </div>
+                    
+                    <br/>
+                    <label>
+                        Results:
+                        <textarea value={this.state.results} disabled cols="30" rows="5" />
+                    </label>
+                </form>
+            </div>    
 
         )
 
--- client/src/tracker/tracker.js
+++ client/src/tracker/tracker.js
@@ -6,59 +6,65 @@ class NutritionTracker extends React.Component{
         super()
         this.state={
             apiValues: {protein: null, carbs: null, fats: null},
-            todayValues: {protein: null, carbs: null, fats: null},
+            todayValues: {protein: 0, carbs: 0, fats: 0},
+            goalValues: {protein: 0, carbs: 0, fats: 0},
+            userInput: ""
         }
-        this.handleClick = this.handleClick.bind(this)
+        this.handleAnalysis = this.handleAnalysis.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)    
+        this.handleChange = this.handleChange.bind(this)     
+        //  need to bind componentDidMount????
     }
 
     componentDidMount(){
         //  load relevant data from database
         //  set state accordingly via this.setState({})
+        //  load api????
     }
 
-    handleClick(e){
-    
-        let id = e.target.id
+    handleChange(e){
+        this.setState({userInput: e.target.value})
+    }
 
-        if (id === "analyze"){
-            //  grab user input
-            //  make api call
-            //  set state with apiValues
-            //  set text appropriately on screen
-        }
-
-        else if (id === "submit"){
-            //  store today's values for macros + apiValues in database
-            //  update state accordingly
-        }
+    handleAnalysis(){
+        
+        //  api operations
 
     }
+
+    //  function handles text changes in textarea
+        //  allows changes to appear on webpage in the textarea
+    handleSubmit(){
+
+        //  submit values to database
+        //  update state's todayValues
+    }
+   
 
     render(){
 
         return(
-            <div className = "NutritionTracker">
+            <div className="NutritionTracker">
                 <h1>Nutrition Tracker</h1>
-                <div className="dailyGoals">
-                    <h2>Daily Goals</h2>
-                    <h3>Protein: </h3>
-                    <h3>Carbs: </h3>
-                    <h3>Fats: </h3>
+                <div className="DailyGoals">
+                    <h3><u>Daily Goals</u></h3>
+                    <p>Protein: {this.state.goalValues.protein}g</p>
+                    <p>Carbs: {this.state.goalValues.carbs}g</p>
+                    <p>Fats: {this.state.goalValues.fats}g</p>
                 </div>
-                    
-                <div className="today">
-                    <h2>Today</h2>
-                    <h3>Protein: </h3>
-                    <h3>Carbs: </h3>
-                    <h3>Fats: </h3>
+                <div className="ProgressToday">
+                    <h3><u>Progress Today</u></h3>
+                    <p>Protein: {this.state.todayValues.protein}g</p>
+                    <p>Carbs: {this.state.todayValues.carbs}g</p>
+                    <p>Fats: {this.state.todayValues.fats}g</p>
                 </div>
-                    
-                <div className="analysis">
-                    <input id="mealInput" type="text"></input>
-                    <p>Input Your Meal</p>
-                    <button type="button" id="analyze" onClick={this.handleClick}>Analyze Meal</button> 
-                    <input id="results" disabled type="text"></input>
-                    <button type="button" id="submit" onClick={this.handleClick}>Submit</button> 
+                <div className="APIBlock">
+                    <h3>Input Meal:</h3>
+                    <textarea cols="50" rows="10" name="user_meal" value={this.state.userInput} onChange={this.handleChange}></textarea>
+                    <button name="analyze" onClick={this.handleAnalysis}>analyze</button>
+                    <h3>Analysis:</h3>
+                    <textarea cols="30" rows="5" name="api_analysis" disabled></textarea>
+                    <button name="submit" onClick={this.handleSubmit}>submit</button>
                 </div>
             </div>
         )
