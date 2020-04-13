244bdd5dd121e7dcd7ec4b4e77855a60bd6816b6
Justin Thomas <jthoma59@uic.edu>
2020-04-13 15:10:07 -0500

    key

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,7 +1,6 @@
 module.exports = {
-    MONGO_URI: 'mongodb://mongo:27017/user',                // connecting to mongodb using docker
-    MONGO_URI_TEST: 'mongodb://mongo:27017/test',       // connect locally for any tests (requires mongod running)
+    MONGO_URI: 'mongodb://127.0.0.1:27017/user',                // connecting to mongodb using docker
+    MONGO_URI_TEST: 'mongodb://127.0.0.1:27017/test',       // connect locally for any tests (requires mongod running)
     SESSION_SECRET: 'ThisIsMySecret',
-    api_app_id: '60c24552',
-    api_app_key: '753490dcad10131dba2d9c2b0a64b79f'
+    
 };
