064b829f3d23dd6c20b6e1a7999bb6cb3fba5021
Justin Thomas <jthoma59@uic.edu>
2020-04-13 15:03:08 -0500

    add route for mealplanner and fixed keys

--- server/routes/index.js
+++ server/routes/index.js
@@ -500,7 +500,7 @@ router.post('/generate-meal-plan', function(req, res){
         return res.json({errMsg: "You need to calculate your recommended calories"})
       }
     })
-    .catch((err) => { console.log("error when retrieving calories") });
+    .catch((err) => { console.log("error during check of calories in generate-meal-plan") });
 
 
   //  extract user entered data
@@ -531,9 +531,10 @@ router.post('/generate-meal-plan', function(req, res){
   //  combine to create url for API call
   url = url + calsUrl + timeFrameUrl + dietUrl + excludeUrl
 
-
-
   /*
+  THIS IS A MOCK API RESPONSE
+    - use if testing API functionality, without needing to call API
+    
   let apiResponse = {
     "meals": [
         {
@@ -574,36 +575,8 @@ router.post('/generate-meal-plan', function(req, res){
         "protein": 55.43
     }
 }
-
-
-  //  save nutrients from apiResponse
-  let _nutrients = []
-  _nutrients.push((apiResponse.nutrients).calories)
-  _nutrients.push((apiResponse.nutrients).carbohydrates)
-  _nutrients.push((apiResponse.nutrients).fat)
-  _nutrients.push((apiResponse.nutrients).protein)
-
-  //  save all meals from apiResponse
-  let _meals = apiResponse.meals
-
-  Rec.findOneAndUpdate( {username: (req.session.user).toString()}, 
-    { "$set": { meals: _meals, mealplan_nutrients: _nutrients}}
-  ).then(
-    console.log("Success")
-  )
-  */
-
-  /*
-  //  use find one to see if they exist in database
-  Rec.findOne( {username: (req.session.user).toString()} )
-    .then((data) => {
-      console.log(data)
-    })
-    .catch((err) => { console.log("error when retrieving calories") });
-  */
-
-  
-    //  send request to API
+*/
+  //  send request to API
   fetch(url, {
     method: 'GET',
     headers: {
@@ -613,30 +586,52 @@ router.post('/generate-meal-plan', function(req, res){
   .then((response) => response.json())
   .then((apiResponse) => {
     
-    //  save nutrients from apiResponse
-  let _nutrients = []
-  _nutrients.push((apiResponse.nutrients).calories)
-  _nutrients.push((apiResponse.nutrients).carbohydrates)
-  _nutrients.push((apiResponse.nutrients).fat)
-  _nutrients.push((apiResponse.nutrients).protein)
+      //  save nutrients from apiResponse
+    let _nutrients = []
+    _nutrients.push((apiResponse.nutrients).calories)
+    _nutrients.push((apiResponse.nutrients).carbohydrates)
+    _nutrients.push((apiResponse.nutrients).fat)
+    _nutrients.push((apiResponse.nutrients).protein)
 
-  //  save all meals from apiResponse
-  let _meals = apiResponse.meals
+    //  save all meals from apiResponse
+    let _meals = apiResponse.meals
 
-  Rec.findOneAndUpdate( {username: (req.session.user).toString()}, 
-    { "$set": { meals: _meals, mealplan_nutrients: _nutrients}}
-  ).then(
-    console.log("Successfully Saved Meal Plan")
-  )
-
+    Rec.findOneAndUpdate( {username: (req.session.user).toString()}, 
+      { "$set": { meals: _meals, mealplan_nutrients: _nutrients}}
+    ).then(
+      console.log("Successfully Saved Meal Plan")
+    )
   })
   .catch((error) => {
-    console.error('Error:', error);
+    console.log('Error when saving meal plan');
   });
   
-  
+})
+
+
+router.get('/get-meal-plan', function(req, res){
+
+  if (!req.session.user || req.session.user === undefined) {
+    return res.json({errMsg: 'You must be logged in to do that!'});
+  }
+
+  Rec.findOne( {username: (req.session.user).toString()} )
+    .then((data) => {
+      if (data.meals == null){
+        return res.json({errMsg: "You haven't generated your meal plan yet!"})
+      }
+      
+      let _bfast = {name: ((data.meals[0]).title), readyIn: ((data.meals[0]).readyInMinutes), servings: ((data.meals[0]).servings)};
+      let _lunch = {name: ((data.meals[1]).title), readyIn: ((data.meals[1]).readyInMinutes), servings: ((data.meals[1]).servings)};
+      let _dinner = {name: ((data.meals[2]).title), readyIn: ((data.meals[2]).readyInMinutes), servings: ((data.meals[2]).servings)};
+      let _nutrients = {calories: ((data.mealplan_nutrients)[0]), carbs: ((data.mealplan_nutrients)[1]), fat: ((data.mealplan_nutrients)[2]), protein: ((data.mealplan_nutrients)[3])}
+      return res.json({bfast: _bfast, lunch: _lunch, dinner: _dinner, nutrients: _nutrients})
+    })
+    .catch((err) => { console.log("error when getting meal plan") });
+
 
 })
+
 
 
 module.exports = router;
