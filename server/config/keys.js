1774d2d0728a3d430793d8129bafef59ff1068d4
Justin Thomas <jthoma59@uic.edu>
2020-04-22 17:55:50 -0500

    add uri

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,6 +1,8 @@
 module.exports = {
-  MONGO_URI: 'mongodb://mongo:27017/user', // connecting to mongodb using docker
-  MONGO_URI_TEST: 'mongodb://mongo:27017/test', // connect locally for any tests (requires mongod running)
+  MONGO_URI: 'mongodb://127.0.0.1:27017/user', // connecting to mongodb using docker
+  MONGO_URI_TEST: 'mongodb://127.0.0.1:27017/test', // connect locally for any tests (requires mongod running)
   SESSION_SECRET: 'ThisIsMySecret',
   app_key: 'fe018186f98a4236b46286e507ac061a',
 };
+
+//  127.0.0.1
